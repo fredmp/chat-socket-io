bc09ce789b7db546f77912ccc28b6ac6de0afc01
Frederico Martins Porto <martinsporto@gmail.com>
2018-01-15 12:07:11 -0200

    Broadcasting events

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ io.on('connection', socket => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user has joined'
+  });
+
+  socket.emit('newMessage', {
     from: 'me',
     text: 'Some text',
     createAt: new Date().getTime()
@@ -22,6 +31,13 @@ io.on('connection', socket => {
 
   socket.on('createMessage', (data) => {
     console.log('A new message was created', data);
+    data.createdAt = new Date().getTime();
+
+    // Emits message to everyone
+    // io.emit('newMessage', data);
+
+    // Emits message to everyone except sender
+    socket.broadcast.emit('newMessage', data);
   });
 
   socket.on('disconnect', () => {
